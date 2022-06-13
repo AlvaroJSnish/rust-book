f82789cc9562261703be95e27e6f264b9122e78f
Álvaro <alvarohdezex@gmail.com>
2022-06-13 23:47:47 +0200

    1.2 - hello cargo: compilando y ejecutando el proyecto con cargo

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\alvaro\Desktop\rust\hello_cargo\target\debug\hello_cargo.exe: C:\Users\alvaro\Desktop\rust\hello_cargo\src\main.rs
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\alvaro\Desktop\rust\hello_cargo\target\release\hello_cargo.exe: C:\Users\alvaro\Desktop\rust\hello_cargo\src\main.rs
